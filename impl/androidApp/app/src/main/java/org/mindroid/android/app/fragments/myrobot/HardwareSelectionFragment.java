ddd34584ae5a4a534480fc63ef1113593d6ad34c
Torben Unzicker <Torben_U@gmx.net>
2017-05-22 12:43:41 +0200

    Motor/Senosr Configuration will be saved now

--- impl/androidApp/app/src/main/java/org/mindroid/android/app/fragments/myrobot/HardwareSelectionFragment.java
+++ impl/androidApp/app/src/main/java/org/mindroid/android/app/fragments/myrobot/HardwareSelectionFragment.java
@@ -1,6 +1,7 @@
 package org.mindroid.android.app.fragments.myrobot;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -25,6 +26,7 @@ import static org.mindroid.common.messages.Sensors.EV3GyroSensor;
 import static org.mindroid.common.messages.Sensors.EV3IRSensor;
 import static org.mindroid.common.messages.Sensors.EV3TouchSensor;
 import static org.mindroid.common.messages.Sensors.EV3UltrasonicSensor;
+import static org.mindroid.common.messages.Sensors.getAllSensorTypes;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -75,6 +77,8 @@ public class HardwareSelectionFragment extends Fragment implements MyRobotFragme
     private Spinner spinner_select_type;
     private Spinner spinner_select_mode;
     private TextView txtView_port;
+    private TextView txtView_type;
+    private TextView txtView_mode;
 
 
 
@@ -115,9 +119,13 @@ public class HardwareSelectionFragment extends Fragment implements MyRobotFragme
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_hardware_selection, container, false);
 
+
+
         spinner_select_type = (Spinner) view.findViewById(R.id.spinner_type);
         spinner_select_mode = (Spinner) view.findViewById(R.id.spinner_mode);
         txtView_port = (TextView) view.findViewById(R.id.txtView_port);
+        txtView_type = (TextView) view.findViewById(R.id.txtView_hardwareSelect_type);
+        txtView_mode = (TextView) view.findViewById(R.id.txtView_hardwareSelect_mode);
 
         switch(hardware_selection_mode){
             case HARDWARE_SELECTION_MODE_MOTOR: initMotorView(); break;
@@ -134,9 +142,8 @@ public class HardwareSelectionFragment extends Fragment implements MyRobotFragme
                     spinner_select_mode.setAdapter(getSensorModeAdapter(selectedSensorType));
                 }
                 if(isValidSelection()){
-                    Toast.makeText(getActivity(),"Valid configuration",Toast.LENGTH_SHORT).show();
-                    //TODO check if config changed --> than save
-                    // MOTOR AND SENSOR!!!!! compare mode
+                    //Toast.makeText(getActivity(),"Valid configuration",Toast.LENGTH_SHORT).show();
+                    savePortConfig(hardware_selection_mode, hardwarePort);
                 }
             }
 
@@ -145,6 +152,8 @@ public class HardwareSelectionFragment extends Fragment implements MyRobotFragme
                 selectedSensorType = notDefined;
             }
         });
+
+        loadPortConfig(hardware_selection_mode,hardwarePort);
 
         return view;
     }
@@ -162,6 +171,9 @@ public class HardwareSelectionFragment extends Fragment implements MyRobotFragme
             txtView_port.setText("4");
         }
 
+        txtView_type.setText(getResources().getString(R.string.view_sensortype));
+        txtView_mode.setText(getResources().getString(R.string.view_sensormode));
+
         spinner_select_type.setAdapter(getSensorTypeAdapter());
         spinner_select_mode.setAdapter(getSensorModeAdapter(notDefined));
 
@@ -170,8 +182,8 @@ public class HardwareSelectionFragment extends Fragment implements MyRobotFragme
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 selectedSensorMode = (String) parent.getSelectedItem();
                 if(isValidSelection()){
-                    Toast.makeText(getActivity(),"Valid configuration",Toast.LENGTH_SHORT).show();
-                    //TODO check if config changed --> than save
+                    //Toast.makeText(getActivity(),"Valid configuration",Toast.LENGTH_SHORT).show();
+                    savePortConfig(hardware_selection_mode, hardwarePort);
                 }
             }
 
@@ -195,6 +207,9 @@ public class HardwareSelectionFragment extends Fragment implements MyRobotFragme
                     }
                 }
                 return false;
+            }else if(selectedSensorType.equals(notDefined) && selectedSensorMode.equals(notDefined)){
+                //No Sensor-/Mode selected
+                return true;
             }
         }else{
             //Motor Hardware Selection mode --> always true
@@ -205,7 +220,7 @@ public class HardwareSelectionFragment extends Fragment implements MyRobotFragme
 
     private void initSensorTypeToModeMap() {
         senTypeToMode = new HashMap<String,String[]>();
-        senTypeToMode.put(notDefined,getSensorModes());
+        senTypeToMode.put(notDefined,new String[]{notDefined});
 
         for (Sensors sensors : Sensors.getAllSensorTypes()) {
             String[] modes = new String[sensors.getModes().length];
@@ -251,6 +266,9 @@ public class HardwareSelectionFragment extends Fragment implements MyRobotFragme
         }else if(hardwarePort.equals(getResources().getString(R.string.KEY_MOTOR_D))){
             txtView_port.setText("D");
         }
+
+        txtView_type.setText(getResources().getString(R.string.view_motortype));
+        txtView_mode.setVisibility(View.GONE);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -277,13 +295,106 @@ public class HardwareSelectionFragment extends Fragment implements MyRobotFragme
         mListener = null;
     }
 
+    private void loadPortConfig(int hardware_selection_mode, String hardwarePort){
+        SharedPreferences portConfigProperties = getActivity().getApplicationContext().getSharedPreferences(getResources().getString(R.string.shared_pref_portConfiguration),Context.MODE_PRIVATE);
+        if(portConfigProperties != null) {
+            // ---- load sensortypes ---- //
+            String type ="";
+            String sensormode="";
+
+            if(hardware_selection_mode == HARDWARE_SELECTION_MODE_SENSOR) {
+                //SENSOR
+                if (hardwarePort.equals(getResources().getString(R.string.KEY_SENSOR_S1))) {
+                    type = portConfigProperties.getString(getResources().getString(R.string.KEY_SENSOR_S1), notDefined);
+                    sensormode = portConfigProperties.getString(getResources().getString(R.string.KEY_SENSORMODE_S1), notDefined);
+                } else if (hardwarePort.equals(getResources().getString(R.string.KEY_SENSOR_S2))) {
+                    type = portConfigProperties.getString(getResources().getString(R.string.KEY_SENSOR_S2), notDefined);
+                    sensormode = portConfigProperties.getString(getResources().getString(R.string.KEY_SENSORMODE_S2), notDefined);
+                } else if (hardwarePort.equals(getResources().getString(R.string.KEY_SENSOR_S3))) {
+                    type = portConfigProperties.getString(getResources().getString(R.string.KEY_SENSOR_S3), notDefined);
+                    sensormode = portConfigProperties.getString(getResources().getString(R.string.KEY_SENSORMODE_S3), notDefined);
+                } else if (hardwarePort.equals(getResources().getString(R.string.KEY_SENSOR_S4))) {
+                    type = portConfigProperties.getString(getResources().getString(R.string.KEY_SENSOR_S4), notDefined);
+                    sensormode = portConfigProperties.getString(getResources().getString(R.string.KEY_SENSORMODE_S4), notDefined);
+                }
+
+                selectSpinnerItem(spinner_select_type,type);
+                selectSpinnerItem(spinner_select_mode,sensormode);
+
+            }else{
+                //MOTOR
+                if (hardwarePort.equals(getResources().getString(R.string.KEY_MOTOR_A))) {
+                    type = portConfigProperties.getString(getResources().getString(R.string.KEY_MOTOR_A), notDefined);
+
+                } else if (hardwarePort.equals(getResources().getString(R.string.KEY_MOTOR_B))) {
+                    type = portConfigProperties.getString(getResources().getString(R.string.KEY_MOTOR_B), notDefined);
+
+                } else if (hardwarePort.equals(getResources().getString(R.string.KEY_MOTOR_C))) {
+                    type = portConfigProperties.getString(getResources().getString(R.string.KEY_MOTOR_C), notDefined);
+
+                } else if (hardwarePort.equals(getResources().getString(R.string.KEY_MOTOR_D))) {
+                    type = portConfigProperties.getString(getResources().getString(R.string.KEY_MOTOR_D), notDefined);
+                }
+
+                selectSpinnerItem(spinner_select_type,type);
+
+            }
+        }
+    }
+
+    private void selectSpinnerItem(Spinner spinner, String item){
+        for(int i = 0; i < spinner.getAdapter().getCount();i++){
+            if(spinner.getItemAtPosition(i).equals(item)){
+                spinner.setSelection(i);
+                break;
+            }
+        }
+    }
+
+    private void savePortConfig(int hardware_selection_mode, String hardwarePort){
+        SharedPreferences.Editor e = getActivity().getApplicationContext().getSharedPreferences(getResources().getString(R.string.shared_pref_portConfiguration),Context.MODE_PRIVATE).edit();
+        //e.clear();
+        if(e != null) {
+            if(hardware_selection_mode == HARDWARE_SELECTION_MODE_SENSOR) {
+                //SENSOR
+                if (hardwarePort.equals(getResources().getString(R.string.KEY_SENSOR_S1))) {
+                    e.putString(getResources().getString(R.string.KEY_SENSOR_S1),spinner_select_type.getSelectedItem().toString());
+                    e.putString(getResources().getString(R.string.KEY_SENSORMODE_S1),spinner_select_mode.getSelectedItem().toString());
+                } else if (hardwarePort.equals(getResources().getString(R.string.KEY_SENSOR_S2))) {
+                    e.putString(getResources().getString(R.string.KEY_SENSOR_S2),spinner_select_type.getSelectedItem().toString());
+                    e.putString(getResources().getString(R.string.KEY_SENSORMODE_S2),spinner_select_mode.getSelectedItem().toString());
+                } else if (hardwarePort.equals(getResources().getString(R.string.KEY_SENSOR_S3))) {
+                    e.putString(getResources().getString(R.string.KEY_SENSOR_S3),spinner_select_type.getSelectedItem().toString());
+                    e.putString(getResources().getString(R.string.KEY_SENSORMODE_S3),spinner_select_mode.getSelectedItem().toString());
+                } else if (hardwarePort.equals(getResources().getString(R.string.KEY_SENSOR_S4))) {
+                    e.putString(getResources().getString(R.string.KEY_SENSOR_S4),spinner_select_type.getSelectedItem().toString());
+                    e.putString(getResources().getString(R.string.KEY_SENSORMODE_S4),spinner_select_mode.getSelectedItem().toString());
+                }
+
+               e.commit();
+            }else{
+                //MOTOR
+                if (hardwarePort.equals(getResources().getString(R.string.KEY_MOTOR_A))) {
+                    e.putString(getResources().getString(R.string.KEY_MOTOR_A),spinner_select_type.getSelectedItem().toString());
+
+                } else if (hardwarePort.equals(getResources().getString(R.string.KEY_MOTOR_B))) {
+                    e.putString(getResources().getString(R.string.KEY_MOTOR_B),spinner_select_type.getSelectedItem().toString());
+
+                } else if (hardwarePort.equals(getResources().getString(R.string.KEY_MOTOR_C))) {
+                    e.putString(getResources().getString(R.string.KEY_MOTOR_C),spinner_select_type.getSelectedItem().toString());
+
+                } else if (hardwarePort.equals(getResources().getString(R.string.KEY_MOTOR_D))) {
+                    e.putString(getResources().getString(R.string.KEY_MOTOR_D),spinner_select_type.getSelectedItem().toString());
+
+                }
+                e.commit();
+            }
+        }
+    }
+
     @Override
     public void onFragmentInteraction(Uri uri) {
 
-    }
-
-    public String[] getSensorModes() {
-        return Sensors.getAllModes();
     }
 
     /**

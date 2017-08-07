1dd150efaf6edab923a17cdab960c9469b6fdacd
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2017-08-07 23:50:38 +0200

    Fix compiler errors in Robot.java

--- impl/androidApp/app/src/main/java/org/mindroid/android/app/SchuelerProjekt/Robot.java
+++ impl/androidApp/app/src/main/java/org/mindroid/android/app/SchuelerProjekt/Robot.java
@@ -1,15 +1,20 @@
 package org.mindroid.android.app.SchuelerProjekt;
 
 import org.mindroid.api.LVL2API;
+import org.mindroid.api.statemachine.exception.StateAlreadyExists;
 
 public class Robot extends LVL2API {
+
+    public Robot() throws StateAlreadyExists {
+      
+    }
 
     @Override
     public void run() {
         
         sendLogMessage("Implementation is running!");
 
-        brickController.drawString("Hello World!");
+        brickController.drawString("Hello World!", 1, 1);
         
     }
     

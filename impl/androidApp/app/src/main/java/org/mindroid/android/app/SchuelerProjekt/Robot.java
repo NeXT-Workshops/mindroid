102dd90a18a796163b6c4ed76f48fcee843b399a
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2017-08-10 09:23:32 +0200

    Remove constructor of Robot
    (Not needed anymore)

--- impl/androidApp/app/src/main/java/org/mindroid/android/app/SchuelerProjekt/Robot.java
+++ impl/androidApp/app/src/main/java/org/mindroid/android/app/SchuelerProjekt/Robot.java
@@ -1,13 +1,8 @@
 package org.mindroid.android.app.SchuelerProjekt;
 
 import org.mindroid.api.LVL2API;
-import org.mindroid.api.statemachine.exception.StateAlreadyExists;
 
 public class Robot extends LVL2API {
-
-    public Robot() throws StateAlreadyExists {
-      
-    }
 
     @Override
     public void run() {

75f5eb39cc6f651eda419a2e414816bc6cc665c1
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2017-08-15 15:07:24 +0200

    Improve server application
    - Select wireless network interface (best effort, fully portable solution not found yet)
    - Use Log4J

--- impl/serverApp/src/main/java/org/mindroid/server/app/MindroidServerApplicationMain.java
+++ impl/serverApp/src/main/java/org/mindroid/server/app/MindroidServerApplicationMain.java
@@ -1,11 +1,17 @@
 package org.mindroid.server.app;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.awt.*;
-import java.io.File;
 import java.io.IOException;
-import java.net.*;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Enumeration;
 
 /**
@@ -15,129 +21,132 @@ import java.util.Enumeration;
  */
 public class MindroidServerApplicationMain {
 
-	private static MindroidServerFrame mindroidServerFrame;
-	private final static int port = 33044;
-	private static ServerSocket server = null;
+    private static final Logger logger = LogManager.getLogger(MindroidServerApplicationMain.class);
+    private final static int port = 33044;
+    private static MindroidServerFrame mindroidServerFrame;
+    private static ServerSocket server = null;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.setProperty("apple.laf.useScreenMenuBar","true");
-		mindroidServerFrame = new MindroidServerFrame();
+        System.setProperty("apple.laf.useScreenMenuBar", "true");
+        mindroidServerFrame = new MindroidServerFrame();
         try {
-			runServer();
-		} catch (Exception e) {
-			MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
-			console.setVisible(true);
-			console.appendLine("Unknown Exception. Please restart the Application. Exception Message: "+ e.getMessage());
-			console.appendLine(e.toString());
-			mindroidServerFrame.addContentLine("Local","ERROR","Server not running.");
-			mindroidServerFrame.addContentLine("Local","INFO","See Error console.");
-			mindroidServerFrame.disableRefresh(true);
-		}
+            runServer();
+        } catch (Exception e) {
+            MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
+            console.setVisible(true);
+            console.appendLine("Unknown Exception. Please restart the Application. Exception Message: " + e.getMessage());
+            console.appendLine(e.toString());
+            mindroidServerFrame.addContentLine("Local", "ERROR", "Server not running.");
+            mindroidServerFrame.addContentLine("Local", "INFO", "See Error console.");
+            mindroidServerFrame.disableRefresh(true);
+        }
 
-	}
+    }
 
-	public static void runServer() {
-		try{
-			server = new ServerSocket(port);
-			invokeDisplayIPAdress();
-			mindroidServerFrame.addContentLine("Local","INFO","Server started");
-		} catch (IOException e) {
-			MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
-			console.setVisible(true);
-			console.appendLine("The Server could not be started. Try restarting the Application.");
-			console.appendLine(e.toString() + "\n");
-			mindroidServerFrame.addContentLine("Local","ERROR","Server not running.");
-			mindroidServerFrame.addContentLine("Local","INFO","See Error console.");
-			mindroidServerFrame.disableRefresh(true);
-			return;
-		}
+    public static void runServer() {
+        try {
+            server = new ServerSocket(port);
+            invokeDisplayIPAdress();
+            mindroidServerFrame.addContentLine("Local", "INFO", "Server started");
+        } catch (IOException e) {
+            MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
+            console.setVisible(true);
+            console.appendLine("The Server could not be started. Try restarting the Application.");
+            console.appendLine(e.toString() + "\n");
+            mindroidServerFrame.addContentLine("Local", "ERROR", "Server not running.");
+            mindroidServerFrame.addContentLine("Local", "INFO", "See Error console.");
+            mindroidServerFrame.disableRefresh(true);
+            return;
+        }
 
-		Thread.UncaughtExceptionHandler exceptionHandler = new Thread.UncaughtExceptionHandler() {
-			public void uncaughtException(Thread th, Throwable ex) {
-				MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
-				console.setVisible(true);
-				console.appendLine("Messages from one connection might have been lost. No need to restart the application.");
-				console.appendLine(ex.toString());
-			}
-		};
+        Thread.UncaughtExceptionHandler exceptionHandler = new Thread.UncaughtExceptionHandler() {
+            public void uncaughtException(Thread th, Throwable ex) {
+                MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
+                console.setVisible(true);
+                console.appendLine("Messages from one connection might have been lost. No need to restart the application.");
+                console.appendLine(ex.toString());
+            }
+        };
 
-		int iteration = 0;
-		while(true){
-			++iteration;
-			System.out.println("[C# " + iteration + "] Waiting...");
-			MindroidServerWorker w;
-			try{
-				w = new MindroidServerWorker(server.accept(), mindroidServerFrame);
-				Thread t = new Thread(w);
-				t.setUncaughtExceptionHandler(exceptionHandler);
-				t.start();
-			} catch (IOException e) {
-				MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
-				console.setVisible(true);
-				console.appendLine("Error while receiving a message.");
-				console.appendLine("IOException: "+e.getMessage()+"\n");
-				return;
-			}
-		}
-	}
-
-
-	public static void invokeDisplayIPAdress() {
-
-		Enumeration networkInterfaces = null;
-		try {
-			networkInterfaces = NetworkInterface.getNetworkInterfaces();
-		} catch (SocketException e1) {
-			e1.printStackTrace();
-			mindroidServerFrame.displayIPAdress("Check connection.", Color.RED);
-			MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
-			console.setVisible(true);
-			console.appendLine("Check your connection to the router.");
-			console.appendLine("Try refreshing the IP Address (in File Menu). \n");
-		}
-
-		InetAddress loopback = null;
-		InetAddress privateIP = null;
-		while(networkInterfaces.hasMoreElements())
-		{
-			NetworkInterface n = (NetworkInterface) networkInterfaces.nextElement();
-			Enumeration addresses = n.getInetAddresses();
-			while (addresses.hasMoreElements())
-			{
-				InetAddress address = (InetAddress) addresses.nextElement();
-
-				if (address.isLoopbackAddress()) {
-					loopback = address;
-				}
-				if (address.isSiteLocalAddress()) {
-					privateIP = address;
-				}
-			}
-
-		}
-		if (privateIP!=null) {
-			mindroidServerFrame.displayIPAdress(privateIP.getHostAddress(),Color.BLACK);
-			MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
-			console.appendLine("Connected to the router.");
-			console.appendLine("Server IP Address: "+ privateIP.getHostAddress() +"\n");
-		} else if (loopback!=null) {
-			mindroidServerFrame.displayIPAdress(loopback.getHostAddress(),Color.RED);
-			MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
-			console.setVisible(true);
-			console.appendLine("Check your connection to the router. Only a loopback address was found.");
-			console.appendLine("Try refreshing the IP Address (in File Menu). \n");
-		} else {
-			mindroidServerFrame.displayIPAdress("Check connection.", Color.RED);
-			MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
-			console.setVisible(true);
-			console.appendLine("Check your connection to the router.");
-			console.appendLine("Try refreshing the IP Address (in File Menu). \n");
-		}
+        int iteration = 0;
+        while (true) {
+            ++iteration;
+            logger.info("[C# " + iteration + "] Waiting...");
+            MindroidServerWorker w;
+            try {
+                w = new MindroidServerWorker(server.accept(), mindroidServerFrame);
+                Thread t = new Thread(w);
+                t.setUncaughtExceptionHandler(exceptionHandler);
+                t.start();
+            } catch (IOException e) {
+                MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
+                console.setVisible(true);
+                console.appendLine("Error while receiving a message.");
+                console.appendLine("IOException: " + e.getMessage() + "\n");
+                return;
+            }
+        }
+    }
 
 
+    public static void invokeDisplayIPAdress() {
+        try {
+            Enumeration networkInterfaces = NetworkInterface.getNetworkInterfaces();
 
-	}
+            InetAddress loopback = null;
+            List<InetAddress> privateIP = new ArrayList<>();
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface n = (NetworkInterface) networkInterfaces.nextElement();
+                Enumeration addresses = n.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    InetAddress address = (InetAddress) addresses.nextElement();
+
+                    if (address.isLoopbackAddress()) {
+                        loopback = address;
+                    }
+
+                    // The following filter has only been tested on Windows 8.1
+                    if (address.isSiteLocalAddress() && isWirelessInterface(n)) {
+                        privateIP.add(address);
+                    }
+                }
+
+            }
+            if (!privateIP.isEmpty()) {
+                mindroidServerFrame.displayIPAdress(privateIP.get(0).getHostAddress(), Color.BLACK);
+                MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
+                console.appendLine("Available IP addresses: " + privateIP.toString());
+                console.appendLine("Connected to the router.");
+                console.appendLine("Server IP Address: " + privateIP.get(0).getHostAddress() + "\n");
+            } else if (loopback != null) {
+                mindroidServerFrame.displayIPAdress(loopback.getHostAddress(), Color.RED);
+                MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
+                console.setVisible(true);
+                console.appendLine("Check your connection to the router. Only a loopback address was found.");
+                console.appendLine("Try refreshing the IP Address (in File Menu). \n");
+            } else {
+                mindroidServerFrame.displayIPAdress("Check connection.", Color.RED);
+                MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
+                console.setVisible(true);
+                console.appendLine("Check your connection to the router.");
+                console.appendLine("Try refreshing the IP Address (in File Menu). \n");
+            }
+
+        } catch (SocketException e1) {
+            e1.printStackTrace();
+            mindroidServerFrame.displayIPAdress("Check connection.", Color.RED);
+            MindroidServerConsole console = MindroidServerConsole.getMindroidServerConsole();
+            console.setVisible(true);
+            console.appendLine("Check your connection to the router.");
+            console.appendLine("Try refreshing the IP Address (in File Menu). \n");
+        }
+
+    }
+
+    private static boolean isWirelessInterface(NetworkInterface n) {
+        return n.getName().contains("wlan") || n.getDisplayName().toLowerCase().contains("wireless");
+    }
 
 
 }

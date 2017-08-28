19f7a48839d317cd61d56de3e9fdbc56ea4877c1
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2017-08-28 15:09:22 +0200

    Remove unnecessary imports

--- impl/androidApp/app/src/main/java/org/mindroid/android/app/statemachinesimpl/MindroidLVL2.java
+++ impl/androidApp/app/src/main/java/org/mindroid/android/app/statemachinesimpl/MindroidLVL2.java
@@ -1,8 +1,6 @@
 package org.mindroid.android.app.statemachinesimpl;
 
 import org.mindroid.api.LVL2API;
-import org.mindroid.api.ev3.EV3StatusLightColor;
-import org.mindroid.api.ev3.EV3StatusLightInterval;
 
 public class MindroidLVL2 extends LVL2API {
     @Override

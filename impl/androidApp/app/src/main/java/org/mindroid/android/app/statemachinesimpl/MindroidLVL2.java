3dab31ee763e0aabad8a16e8364a301994de0791
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2017-08-28 14:54:57 +0200

    Set Hello World as default code

--- impl/androidApp/app/src/main/java/org/mindroid/android/app/statemachinesimpl/MindroidLVL2.java
+++ impl/androidApp/app/src/main/java/org/mindroid/android/app/statemachinesimpl/MindroidLVL2.java
@@ -7,25 +7,7 @@ import org.mindroid.api.ev3.EV3StatusLightInterval;
 public class MindroidLVL2 extends LVL2API {
     @Override
     public void run() { 
-         int iteration = 0;
-        while (true  && !isInterrupted()) {
-            forward();
-            while (distanceGreaterThan(0.15f) && !isInterrupted()) {
-                delay(300);
-            }
-            stopMotors();
-            backward();
-            delay(1200);
-            setLED(EV3StatusLightColor.GREEN, EV3StatusLightInterval.BLINKING);
-            if (iteration % 2 == 0) {
-              turnLeft(140);
-            } else {
-              turnRight(140);
-            }
-            
-            setLED(EV3StatusLightColor.OFF, EV3StatusLightInterval.BLINKING);
-            stopMotors();
-            iteration++;
-        }
+        clearDisplay();
+        drawString("Hello World!", 1, 1);
     }
 }

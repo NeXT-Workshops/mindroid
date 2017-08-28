c9b03e7668b80699aeb976932061f247eb39d16e
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2017-08-28 14:54:01 +0200

    GenerateJavadoc only generates docu for androidApp

--- admin/Scripts/GenerateJavadoc.bat
+++ admin/Scripts/GenerateJavadoc.bat
@@ -1,10 +1,11 @@
 @set startdir=%cd%
 @cd ../../impl/androidApp
 @call gradlew.bat javadoc
-@cd ../ev3App
-@call gradlew.bat javadoc
-@cd ../serverApp
-@call gradlew.bat javadoc
-@cd ../include/ev3Messages
-@call gradlew.bat javadoc
+REM @cd ../ev3App
+REM @call gradlew.bat javadoc
+REM @cd ../serverApp
+REM @call gradlew.bat javadoc
+REM @cd ../include/ev3Messages
+REM @call gradlew.bat javadoc
 @cd %startdir%
+pause

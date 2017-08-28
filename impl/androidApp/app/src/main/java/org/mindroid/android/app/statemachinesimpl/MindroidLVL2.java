81d95f38fd8ee36371a75229839497c7704073d6
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2017-08-28 15:13:08 +0200

    Dummy edit

--- impl/androidApp/app/src/main/java/org/mindroid/android/app/statemachinesimpl/MindroidLVL2.java
+++ impl/androidApp/app/src/main/java/org/mindroid/android/app/statemachinesimpl/MindroidLVL2.java
@@ -5,7 +5,9 @@ import org.mindroid.api.LVL2API;
 public class MindroidLVL2 extends LVL2API {
     @Override
     public void run() { 
+        
         clearDisplay();
         drawString("Hello World!", 1, 1);
+        
     }
 }

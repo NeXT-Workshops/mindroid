f6fe6b0bfa90d4b504f7421ee8e253a569b842c6
Torben Unzicker <Torben_U@gmx.net>
2018-02-03 13:36:48 +0100

    Small change on the Colors.getColorByValue call (removed the mapping class) (#105)

--- impl/androidApp/api/src/main/java/org/mindroid/api/ImperativeWorkshopAPI.java
+++ impl/androidApp/api/src/main/java/org/mindroid/api/ImperativeWorkshopAPI.java
@@ -63,7 +63,7 @@ public abstract class ImperativeWorkshopAPI extends ImperativeAPI{
      */
     public final Colors getLeftColor() {
         if(getLeftColorSensor().getSensormode().equals(Sensormode.COLOR_ID)){
-            return Colors.Mapping.getColorByValue(getLeftColorSensor().getValue()[0]);
+            return Colors.getColorByValue(getLeftColorSensor().getValue()[0]);
         }else{
             return Colors.NONE;
         }
@@ -78,7 +78,7 @@ public abstract class ImperativeWorkshopAPI extends ImperativeAPI{
      */
     public Colors getRightColor() {
         if(getRightColorSensor().getSensormode().equals(Sensormode.COLOR_ID)){
-            return Colors.Mapping.getColorByValue(getRightColorSensor().getValue()[0]);
+            return Colors.getColorByValue(getRightColorSensor().getValue()[0]);
         }else{
             return Colors.NONE;
         }
--- impl/androidApp/api/src/main/java/org/mindroid/impl/statemachine/properties/Colors.java
+++ impl/androidApp/api/src/main/java/org/mindroid/impl/statemachine/properties/Colors.java
@@ -26,39 +26,36 @@ public enum Colors {
         return value;
     }
 
-    /**
-     * Contains the Mapping between the measured sensor values and the {@link Colors} Object
-     */
-    public static class Mapping{
-        private static final Map<Float,Colors> FLOAT_COLORS_MAP;
-        static
-        {
-            FLOAT_COLORS_MAP = new HashMap<Float,Colors>();
-            FLOAT_COLORS_MAP.put(NONE.getValue(),    NONE);
-            FLOAT_COLORS_MAP.put(BLACK.getValue(),   BLUE);
-            FLOAT_COLORS_MAP.put(GREEN.getValue(),   GREEN);
-            FLOAT_COLORS_MAP.put(YELLOW.getValue(),  YELLOW);
-            FLOAT_COLORS_MAP.put(RED.getValue(),     RED);
-            FLOAT_COLORS_MAP.put(WHITE.getValue(),   WHITE);
-            FLOAT_COLORS_MAP.put(BROWN.getValue(),   BROWN);
-        }
-
-        /**
-         *  Returns the Colors of the given value.
-         *  NONE = -1f;
-         *  BLACK = 0f;
-         *  BLUE = 1f;
-         *  GREEN = 2f;
-         *  YELLOW = 3f;
-         *  RED = 4f;
-         *  WHITE = 5f;
-         *  BROWN = 6f;
-         *
-         * @param value between -1.0 and 6.0
-         * @return the Colors-object mapped to the value
-         */
-        public static Colors getColorByValue(float value){
-            return FLOAT_COLORS_MAP.get(value);
-        }
+    //Value  to color mapping
+    private static final Map<Float,Colors> FLOAT_COLORS_MAP;
+    static
+    {
+        FLOAT_COLORS_MAP = new HashMap<Float,Colors>();
+        FLOAT_COLORS_MAP.put(NONE.getValue(),    NONE);
+        FLOAT_COLORS_MAP.put(BLACK.getValue(),   BLUE);
+        FLOAT_COLORS_MAP.put(GREEN.getValue(),   GREEN);
+        FLOAT_COLORS_MAP.put(YELLOW.getValue(),  YELLOW);
+        FLOAT_COLORS_MAP.put(RED.getValue(),     RED);
+        FLOAT_COLORS_MAP.put(WHITE.getValue(),   WHITE);
+        FLOAT_COLORS_MAP.put(BROWN.getValue(),   BROWN);
     }
+
+    /**
+     *  Returns the {@link Colors} of the given value.
+     *  NONE = -1f;
+     *  BLACK = 0f;
+     *  BLUE = 1f;
+     *  GREEN = 2f;
+     *  YELLOW = 3f;
+     *  RED = 4f;
+     *  WHITE = 5f;
+     *  BROWN = 6f;
+     *
+     * @param value between -1.0 and 6.0
+     * @return the Colors-object mapped to the value
+     */
+    public static Colors getColorByValue(float value){
+        return FLOAT_COLORS_MAP.get(value);
+    }
+
 }

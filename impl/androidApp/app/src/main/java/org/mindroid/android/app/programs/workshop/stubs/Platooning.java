e012ae3b345208819a6b72972216b8b4431ea22e
Marcel <marcelmannmm@freenet.de>
2018-05-30 17:00:51 +0200

    ws: deleted platooning a/b, added platooning stub

---
+++ impl/androidApp/app/src/main/java/org/mindroid/android/app/programs/workshop/stubs/Platooning.java
@@ -0,0 +1,20 @@
+package org.mindroid.android.app.programs.workshop.stubs;
+
+import org.mindroid.api.ImperativeWorkshopAPI;
+import org.mindroid.api.ev3.EV3StatusLightColor;
+import org.mindroid.api.ev3.EV3StatusLightInterval;
+import org.mindroid.common.messages.server.MindroidMessage;
+import org.mindroid.impl.brick.Button;
+import org.mindroid.impl.brick.Textsize;
+
+public class Platooning extends ImperativeWorkshopAPI {
+
+    public Platooning(){
+        super("Platooning");
+    };
+
+    @Override
+    public void run() {
+
+    }
+}

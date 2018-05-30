5beb582a07c86d41ab9f9ee4fb464bd78ab3357f
Marcel Mann <marcelmannmm@freenet.de>
2018-05-30 15:48:38 +0200

    app: follow template added

---
+++ impl/androidApp/app/src/main/java/org/mindroid/android/app/programs/workshop/solutions/Follow.java
@@ -0,0 +1,14 @@
+package org.mindroid.android.app.programs.workshop.solutions;
+
+import org.mindroid.api.ImperativeWorkshopAPI;
+
+public class Follow extends ImperativeWorkshopAPI {
+
+    public Follow() {
+        super("Follower");
+    }
+
+    @Override
+    public void run() {
+    }
+}

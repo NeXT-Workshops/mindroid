5617d8cb3cdcf28fc68921b3ceb8ed14041d8587
Marcel <marcelmannmm@freenet.de>
2018-06-04 15:29:58 +0200

    ws: lawnMower implemented, needs to be tested

--- impl/androidApp/app/src/main/java/org/mindroid/android/app/programs/workshop/solutions/LawnMower.java
+++ impl/androidApp/app/src/main/java/org/mindroid/android/app/programs/workshop/solutions/LawnMower.java
@@ -1,6 +1,7 @@
 package org.mindroid.android.app.programs.workshop.solutions;
 
 import org.mindroid.api.ImperativeWorkshopAPI;
+import org.mindroid.impl.statemachine.properties.Colors;
 
 public class LawnMower extends ImperativeWorkshopAPI {
 
@@ -10,6 +11,20 @@ public class LawnMower extends ImperativeWorkshopAPI {
 
   @Override
     public void run() {
-        
+        while(!isInterrupted()){
+            setMotorSpeed(200);
+            forward();
+            if(getLeftColor()== Colors.RED && getRightColor() == Colors.RED){
+                driveDistanceBackward(10.0f);
+                turnRight(135);
+            }else if(getLeftColor()== Colors.RED){
+                driveDistanceBackward(10.0f);
+                turnRight(90);
+            }else if(getRightColor()== Colors.RED){
+                driveDistanceBackward(10.0f);
+                turnRight(90);
+            }
+            delay(50);
+        }
     }
 }

f3b56b106e2e13defc525f40d82b29607d04960d
Torben Unzicker <Torben_U@gmx.net>
2018-06-06 15:26:48 +0200

    Revert "Changed gradle files, removed api tests"
    This reverts commit d6bf138ea1b3dce9656efbdd6d8b7ff7083d480d.

---
+++ impl/androidApp/api/src/test/java/TestRobotFactory/RobotHardwareConfigurationTest.java
@@ -0,0 +1,73 @@
+package TestRobotFactory;
+
+import org.mindroid.api.robot.IRobotPortConfig;
+import org.mindroid.common.messages.hardware.Motors;
+import org.mindroid.common.messages.hardware.Sensormode;
+import org.mindroid.common.messages.hardware.Sensors;
+
+/**
+ * Created by torben on 02.03.2017.
+ */
+
+public class RobotHardwareConfigurationTest implements IRobotPortConfig {
+    //------------------ SENSORS ------------------
+    @Override
+    public Sensors getSensorS1() {
+        return Sensors.EV3ColorSensor;
+    }
+
+    @Override
+    public Sensors getSensorS2() {
+        return Sensors.EV3UltrasonicSensor;
+    }
+
+    @Override
+    public Sensors getSensorS3() {
+        return null;
+    }
+
+    @Override
+    public Sensors getSensorS4() {
+        return Sensors.EV3ColorSensor;
+    }
+
+    @Override
+    public Sensormode getSensormodeS1() {
+        return Sensormode.RED;
+    }
+
+    @Override
+    public Sensormode getSensormodeS2() {
+        return Sensormode.DISTANCE;
+    }
+
+    @Override
+    public Sensormode getSensormodeS3() {
+        return null;
+    }
+
+    @Override
+    public Sensormode getSensormodeS4() {
+        return Sensormode.RED;
+    }
+
+    //------------------ MOTORS ------------------
+    @Override
+    public Motors getMotorA() {
+        return Motors.LargeRegulatedMotor;   }
+
+    @Override
+    public Motors getMotorB() {
+        return null; }
+
+    @Override
+    public Motors getMotorC() {
+        return null;
+    }
+
+    @Override
+    public Motors getMotorD() {
+        return Motors.LargeRegulatedMotor;
+    }
+
+}
---
+++ impl/androidApp/api/src/test/java/statemachine/constraints/TestConstraints.java
@@ -0,0 +1,262 @@
+package statemachine.constraints;
+
+import org.junit.Test;
+import org.mindroid.api.robot.context.IRobotContextState;
+import org.mindroid.api.sensor.IEV3SensorEventListener;
+import org.mindroid.api.statemachine.constraints.AbstractLogicOperator;
+import org.mindroid.api.statemachine.constraints.IComparator;
+import org.mindroid.api.statemachine.constraints.IConstraint;
+import org.mindroid.common.messages.hardware.Sensormode;
+import org.mindroid.common.messages.hardware.Sensors;
+import org.mindroid.impl.robot.context.RobotContextState;
+import org.mindroid.impl.ev3.EV3PortIDs;
+import org.mindroid.impl.robot.context.StartCondition;
+import org.mindroid.impl.sensor.EV3SensorEndpoint;
+import org.mindroid.impl.sensor.EV3SensorEvent;
+import org.mindroid.impl.statemachine.State;
+import org.mindroid.impl.statemachine.TimeEvent;
+import org.mindroid.impl.statemachine.constraints.*;
+import org.mindroid.impl.statemachine.properties.Milliseconds;
+import org.mindroid.impl.statemachine.properties.Seconds;
+import org.mindroid.impl.statemachine.properties.sensorproperties.Distance;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Created by torben on 24.03.2017.
+ */
+public class TestConstraints {
+
+    @Test
+    public void testLT(){
+        float[] test_distance = {20};
+        float[] test_value_true = {15};
+        float[] test_value_flase = {25};
+
+        IRobotContextState contextState = new RobotContextState();
+        EV3SensorEndpoint testSensor = new EV3SensorEndpoint("-",1,1, Sensors.EV3UltrasonicSensor,EV3PortIDs.PORT_1, Sensormode.DISTANCE);
+        IEV3SensorEventListener rcs = (IEV3SensorEventListener) contextState;
+        rcs.handleSensorEvent(EV3PortIDs.PORT_1,new EV3SensorEvent(testSensor,test_value_true,0, Sensormode.DISTANCE));
+
+        Distance distance = new Distance( EV3PortIDs.PORT_1);
+        IConstraint lowerThan = new LT(test_distance[0],distance);
+
+        assertTrue(((IComparator) lowerThan).evaluate(contextState));
+
+        rcs.handleSensorEvent(EV3PortIDs.PORT_1,new EV3SensorEvent(testSensor,test_value_flase,0, Sensormode.DISTANCE));
+        assertFalse(((IComparator) lowerThan).evaluate(contextState));
+
+        rcs.handleSensorEvent(EV3PortIDs.PORT_1,new EV3SensorEvent(testSensor,test_value_flase,0, Sensormode.LISTEN));
+        assertFalse(((IComparator) lowerThan).evaluate(contextState));
+
+    }
+
+    @Test
+    public void testGT(){
+        float[] test_distance = {20};
+        float[] test_value_false = {15};
+        float[] test_value_true = {25};
+
+        IRobotContextState contextState = new RobotContextState();
+        EV3SensorEndpoint testSensor = new EV3SensorEndpoint("-",1,1, Sensors.EV3UltrasonicSensor,EV3PortIDs.PORT_1, Sensormode.DISTANCE);
+        IEV3SensorEventListener rcs = (IEV3SensorEventListener) contextState;
+        rcs.handleSensorEvent(EV3PortIDs.PORT_1,new EV3SensorEvent(testSensor,test_value_true,0, Sensormode.DISTANCE));
+
+        Distance distance = new Distance( EV3PortIDs.PORT_1);
+        IConstraint gt = new GT(test_distance[0],distance);
+
+        assertTrue(((IComparator) gt).evaluate(contextState));
+
+        rcs.handleSensorEvent(EV3PortIDs.PORT_1,new EV3SensorEvent(testSensor,test_value_false,0, Sensormode.DISTANCE));
+        assertFalse(((IComparator) gt).evaluate(contextState));
+    }
+
+    @Test
+    public void testEQ(){
+        float[] test_distance = {20};
+        float[] test_value_false = {15};
+        float[] test_value_true = {20};
+
+        IRobotContextState contextState = new RobotContextState();
+        EV3SensorEndpoint testSensor = new EV3SensorEndpoint("-",1,1, Sensors.EV3UltrasonicSensor,EV3PortIDs.PORT_1, Sensormode.DISTANCE);
+        IEV3SensorEventListener rcs = (IEV3SensorEventListener) contextState;
+        rcs.handleSensorEvent(EV3PortIDs.PORT_1,new EV3SensorEvent(testSensor,test_value_true,0, Sensormode.DISTANCE));
+
+        Distance distance = new Distance( EV3PortIDs.PORT_1);
+        IConstraint eq = new EQ(test_distance[0],distance);
+
+        assertTrue(((IComparator) eq).evaluate(contextState));
+
+        rcs.handleSensorEvent(EV3PortIDs.PORT_1,new EV3SensorEvent(testSensor,test_value_false,0, Sensormode.DISTANCE));
+        assertFalse(((IComparator) eq).evaluate(contextState));
+    }
+
+    @Test
+    public void testOR(){
+        float[] test_distance = {20};
+        float[] test_value_s1 = {20};
+        float[] test_value_s2 = {19};
+
+        EV3SensorEndpoint testSensor_1 = new EV3SensorEndpoint("-",1,1, Sensors.EV3UltrasonicSensor,EV3PortIDs.PORT_1, Sensormode.DISTANCE);
+        EV3SensorEndpoint testSensor_2 = new EV3SensorEndpoint("-",1,1, Sensors.EV3UltrasonicSensor,EV3PortIDs.PORT_2, Sensormode.DISTANCE);
+
+        IRobotContextState contextState = new RobotContextState();
+
+        IEV3SensorEventListener rcs = (IEV3SensorEventListener) contextState;
+        rcs.handleSensorEvent(EV3PortIDs.PORT_1,new EV3SensorEvent(testSensor_1,test_value_s1,0, Sensormode.DISTANCE));
+        rcs.handleSensorEvent(EV3PortIDs.PORT_2,new EV3SensorEvent(testSensor_2,test_value_s2,0, Sensormode.DISTANCE));
+
+        Distance distance_s1 = new Distance( EV3PortIDs.PORT_1);
+        IConstraint eq_s1 = new EQ(test_distance[0],distance_s1);
+
+        Distance distance_s2 = new Distance( EV3PortIDs.PORT_2);
+        IConstraint eq_s2 = new EQ(test_distance[0],distance_s2);
+        IConstraint or = new OR(eq_s1,eq_s2);
+        boolean result_s1 = ((IComparator) eq_s1).evaluate(contextState);
+        boolean result_s2 = ((IComparator) eq_s2).evaluate(contextState);
+
+        assertTrue(result_s1);
+        assertFalse(result_s2);
+        assertTrue(((AbstractLogicOperator)or).evaluate(result_s1,result_s2));
+
+
+        rcs.handleSensorEvent(EV3PortIDs.PORT_1,new EV3SensorEvent(testSensor_1,new float[]{test_distance[0]+2},0, Sensormode.DISTANCE));
+        rcs.handleSensorEvent(EV3PortIDs.PORT_2,new EV3SensorEvent(testSensor_2,new float[]{test_distance[0]-2},0, Sensormode.DISTANCE));
+        result_s1 = ((IComparator) eq_s1).evaluate(contextState);
+        result_s2 = ((IComparator) eq_s2).evaluate(contextState);
+        assertFalse(result_s1);
+        assertFalse(result_s2);
+        assertFalse(((AbstractLogicOperator)or).evaluate(result_s1,result_s2));
+    }
+
+    @Test
+    public void testAND(){
+        float[] test_distance = {20};
+        float[] test_value_s1 = {20};
+        float[] test_value_s2 = {19};
+
+        EV3SensorEndpoint testSensor_1 = new EV3SensorEndpoint("-",1,1, Sensors.EV3UltrasonicSensor,EV3PortIDs.PORT_1, Sensormode.DISTANCE);
+        EV3SensorEndpoint testSensor_2 = new EV3SensorEndpoint("-",1,1, Sensors.EV3UltrasonicSensor,EV3PortIDs.PORT_2, Sensormode.DISTANCE);
+
+        IRobotContextState contextState = new RobotContextState();
+
+        IEV3SensorEventListener rcs = (IEV3SensorEventListener) contextState;
+        rcs.handleSensorEvent(EV3PortIDs.PORT_1,new EV3SensorEvent(testSensor_1,test_value_s1,0, Sensormode.DISTANCE));
+        rcs.handleSensorEvent(EV3PortIDs.PORT_2,new EV3SensorEvent(testSensor_2,test_value_s2,0, Sensormode.DISTANCE));
+
+        Distance distance_s1 = new Distance( EV3PortIDs.PORT_1);
+        IConstraint eq_s1 = new EQ(test_distance[0],distance_s1);
+
+        Distance distance_s2 = new Distance( EV3PortIDs.PORT_2);
+        IConstraint eq_s2 = new EQ(test_distance[0],distance_s2);
+        IConstraint and = new AND(eq_s1,eq_s2);
+        boolean result_s1 = ((IComparator) eq_s1).evaluate(contextState);
+        boolean result_s2 = ((IComparator) eq_s2).evaluate(contextState);
+
+        assertTrue(result_s1);
+        assertFalse(result_s2);
+        assertFalse(((AbstractLogicOperator)and).evaluate(result_s1,result_s2));
+
+
+        rcs.handleSensorEvent(EV3PortIDs.PORT_1,new EV3SensorEvent(testSensor_1,test_distance,0, Sensormode.DISTANCE));
+        rcs.handleSensorEvent(EV3PortIDs.PORT_2,new EV3SensorEvent(testSensor_2,test_distance,0, Sensormode.DISTANCE));
+        result_s1 = ((IComparator) eq_s1).evaluate(contextState);
+        result_s2 = ((IComparator) eq_s2).evaluate(contextState);
+        assertTrue(result_s1);
+        assertTrue(result_s2);
+        assertTrue(((AbstractLogicOperator)and).evaluate(result_s1,result_s2));
+    }
+
+    @Test
+    public void testTimeExpiredTrue_Seconds() throws InterruptedException {
+        float time_val_ms = 2000;
+        long time_val_sec = 2;
+
+        State testState = new State("test");
+        testState.activate();
+
+        RobotContextState rcs = new RobotContextState();
+        StartCondition.getInstance().setStateActiveTime(1);
+
+        Seconds sec_prop = new Seconds(time_val_sec);
+        sec_prop.setSource(testState);
+
+        TimeExpired time_sec = new TimeExpired(sec_prop);
+        TimeEvent time_event = new TimeEvent(time_val_ms,testState);
+        rcs.handleTimeEvent(time_event);
+
+        Thread.sleep(2200);
+
+        assertTrue(time_sec.evaluate(rcs));
+
+    }
+
+    @Test
+    public void testTimeExpiredFalse_Seconds() throws InterruptedException {
+        float time_val_ms = 2000;
+        long time_val_sec = 2;
+        RobotContextState rcs = new RobotContextState();
+        StartCondition.getInstance().setStateActiveTime(1);
+        State testState = new State("test");
+        testState.activate();
+
+        Seconds sec_prop = new Seconds(time_val_sec);
+        TimeExpired time_sec = new TimeExpired(sec_prop);
+        TimeEvent time_event = new TimeEvent(time_val_ms,testState);
+
+        rcs.getTimeEvents().clear();
+        rcs.handleTimeEvent(time_event);
+
+        StartCondition.getInstance().setStateActiveTime(1);
+        Thread.sleep(1000);
+
+        assertFalse(time_sec.evaluate(rcs));
+    }
+
+    @Test
+    public void testTimeExpiredTrue_Millis() throws InterruptedException {
+        long time_val_ms = 2000;
+        long time_val_sec = 2;
+
+        State testState = new State("test");
+        testState.activate();
+
+        RobotContextState rcs = new RobotContextState();
+        StartCondition.getInstance().setStateActiveTime(1);
+
+        Milliseconds sec_prop = new Milliseconds(time_val_ms);
+        sec_prop.setSource(testState);
+
+        TimeExpired time_sec = new TimeExpired(sec_prop);
+        TimeEvent time_event = new TimeEvent(time_val_ms,testState);
+        rcs.handleTimeEvent(time_event);
+
+        Thread.sleep(2200);
+
+        assertTrue(time_sec.evaluate(rcs));
+
+    }
+
+    @Test
+    public void testTimeExpiredFalse_Millis() throws InterruptedException {
+        long time_val_ms = 2000;
+        long time_val_sec = 2;
+        RobotContextState rcs = new RobotContextState();
+        StartCondition.getInstance().setStateActiveTime(1);
+        State testState = new State("test");
+        testState.activate();
+
+        Milliseconds sec_prop = new Milliseconds(time_val_ms);
+        TimeExpired time_sec = new TimeExpired(sec_prop);
+        TimeEvent time_event = new TimeEvent(time_val_ms,testState);
+
+        rcs.getTimeEvents().clear();
+        rcs.handleTimeEvent(time_event);
+
+        StartCondition.getInstance().setStateActiveTime(1);
+        Thread.sleep(1000);
+
+        assertFalse(time_sec.evaluate(rcs));
+    }
+}

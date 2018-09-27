00f10374be8aad977b13e6eeb0d45f5a7983e3ae
Marcel Mann <mmann@fs-ist.de>
2018-09-27 18:05:04 +0200

    Destination now extends RobotId

--- impl/include/interRobotMessages/src/main/java/org/mindroid/common/messages/server/Destination.java
+++ impl/include/interRobotMessages/src/main/java/org/mindroid/common/messages/server/Destination.java
@@ -5,36 +5,12 @@ import java.util.Objects;
 /**
  * Created by Felicia Ruppel on 19.04.17.
  */
-public class Destination {
+public class Destination extends RobotId{
 
     public static final Destination SERVER_LOG = new Destination("Server");
     public static final Destination BROADCAST = new Destination("Broadcast");
 
-    private final String value;
-
-    public Destination(final String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    @Override
-    public String toString() {
-        return "Destination [" + value + "]";
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Destination destination = (Destination) o;
-        return Objects.equals(value, destination.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
+    public Destination(String value) {
+        super(value);
     }
 }

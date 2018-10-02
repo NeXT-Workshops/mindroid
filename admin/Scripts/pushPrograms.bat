e31800dd39c41dbeb65f4e17cae478b0812a494a
Marcel <marcelmannmm@freenet.de>
2018-10-02 14:57:40 +0200

    improved Scripts for finding and pushing programs

---
+++ admin/Scripts/pushPrograms.bat
@@ -0,0 +1,18 @@
+@echo off
+@set startdir=%cd%
+
+cd ..\..\impl\ImplementationFinder
+
+:push
+:: push programs.json to all devices
+SETLOCAL ENABLEDELAYEDEXPANSION
+@FOR /F "tokens=1,2 skip=1" %%A IN ('%ANDROID_HOME%\platform-tools\adb devices') DO (
+    @SET IS_DEV=%%B
+if "!IS_DEV!" == "device" (
+	@SET SERIAL=%%A
+	@call %ANDROID_HOME%\platform-tools\adb -s !SERIAL! push programs.json /storage/emulated/0/Mindroid/programs.json
+)
+)
+@ENDLOCAL
+
+@cd %startdir% 
--- admin/Scripts/push_programs_json.bat
+++
@@ -1,2 +0,0 @@
-cd ..\..\impl\androidApp
-adb push programs.json /storage/emulated/0/Mindroid/programs.json

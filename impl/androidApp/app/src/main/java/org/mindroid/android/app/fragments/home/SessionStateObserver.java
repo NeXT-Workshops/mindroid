9471f42e2236bc8325469cd7294df3e651c7224b
Marcel <marcelmannmm@freenet.de>
2018-10-05 13:59:49 +0200

    App: now reacts to stop message being sent from sessionHandler when Session closes #151

--- impl/androidApp/app/src/main/java/org/mindroid/android/app/fragments/home/SessionStateObserver.java
+++ impl/androidApp/app/src/main/java/org/mindroid/android/app/fragments/home/SessionStateObserver.java
@@ -1,7 +1,9 @@
 package org.mindroid.android.app.fragments.home;
 
 import android.os.Bundle;
+import org.mindroid.android.app.activities.MainActivity;
 import org.mindroid.api.AbstractImperativeImplExecutor;
+import org.mindroid.impl.robot.Robot;
 
 public class SessionStateObserver implements AbstractImperativeImplExecutor.SessionStateObserver {
     private static SessionStateObserver ourInstance = new SessionStateObserver();
@@ -11,38 +13,41 @@ public class SessionStateObserver implements AbstractImperativeImplExecutor.Sess
     }
 
     SessionProgressFragment spf;
+    private org.mindroid.android.app.robodancer.Robot robot;
 
-    boolean isReady = false;
-    boolean isStopped = false;
+    private boolean ready = false;
+    private boolean stopped = false;
 
     private SessionStateObserver() {
+        robot = MainActivity.robot;
     }
 
     @Override
     public void updateState(String state, int currentRobotCount, int maxSessionRobots) {
         spf.setProgressState(state,currentRobotCount,maxSessionRobots);
         if(state.equals(READY)){
-            isReady = true;
+            ready = true;
         }
 
         if(state.equals(STOP)){
-            isStopped = true;
+            stopped = true;
         }
     }
 
     @Override
     public void stopExecution() {
-        isReady = false;
-        isStopped = true;
+        ready = false;
+        stopped = true;
+        robot.stopRunningImplementation();
     }
 
     public SessionProgressFragment createSessionProgressDialog(SessionProgressTask parent){
-        isReady = false;
-        isStopped = false;
+        ready = false;
+        stopped = false;
         return spf = SessionProgressFragment.newInstance("Session State",new Bundle(),parent);
     }
 
     public boolean isSessionComplete() {
-        return isReady || isStopped;
+        return ready || stopped;
     }
 }

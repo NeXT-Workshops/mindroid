1f2de13185b084a1d2eee715520b8661df1b2218
lejos13 FB18 <Lejos13@FB18.POOL.E-TECHNIK.TU-DARMSTADT.DE>
2019-05-20 08:15:29 +0200

    removed solution from stub again

--- impl/androidApp/app/src/main/java/org/mindroid/android/app/programs/workshop/stubs/ColorTest.java
+++ impl/androidApp/app/src/main/java/org/mindroid/android/app/programs/workshop/stubs/ColorTest.java
@@ -12,28 +12,7 @@ public class ColorTest extends ImperativeWorkshopAPI {
 
      @Override
     public void run() {       
-        while (!isInterrupted()) { 
-            Colors leftColorValue = getLeftColor();
-            Colors rightColorValue = getRightColor();
-
-            clearDisplay();
-            drawString("Colors", 3);
-            drawString("L: " + describeColor(leftColorValue), 4);
-            drawString("R: " + describeColor(rightColorValue), 5);
-            drawString("Distance: " + getDistance(), 6);
-            delay(500);
-        }
+       
     }
 
-    private static String describeColor(final Colors colorValue) {
-        if (colorValue == Colors.NONE)   return "None";
-        if (colorValue == Colors.BLACK)  return "Black";
-        if (colorValue == Colors.BLUE)   return "Blue";
-        if (colorValue == Colors.GREEN)  return "Green";
-        if (colorValue == Colors.YELLOW) return "Yellow";
-        if (colorValue == Colors.RED)    return "Red";
-        if (colorValue == Colors.WHITE)  return "White";
-        if (colorValue == Colors.BROWN)  return "Brown";
-        return "unknown";
-    }
 }

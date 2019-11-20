341b4652f5b51e0dce0138be6148c506b229fba7
Marcel Mann <marcel.mann@stud.tu-darmstadt.de>
2019-11-20 16:10:15 +0100

    added script for setting up mindroid SD-cards

--- admin/scripts/setupLejos.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sshpass -p 'lejos' ssh -o KexAlgorithms=+diffie-hellman-group1-sha1 -c aes128-cbc root@10.0.1.1 'cd lejos && rm settings.properties && echo "lejos.keyclick_volume=0" >> settings.properties && echo "lejos.volume=50" >> settings.properties && cd config && rm pan.config && echo "USB * * 192.168.42.253 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0" > pan.config' 
-

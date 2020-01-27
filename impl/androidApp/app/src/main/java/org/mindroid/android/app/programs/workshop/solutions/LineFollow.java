c98665330e8be95da74accba2ba071fe257ddbd0
Marcel Mann <marcel.mann@stud.tu-darmstadt.de>
2020-01-27 14:31:18 +0100

    added LineFollow

---
+++ impl/androidApp/app/src/main/java/org/mindroid/android/app/programs/workshop/solutions/LineFollow.java
@@ -0,0 +1,28 @@
+package org.mindroid.android.app.programs.workshop.solutions;
+
+import org.mindroid.api.ImperativeWorkshopAPI;
+import org.mindroid.impl.statemachine.properties.Colors;
+
+public class LineFollow extends ImperativeWorkshopAPI {
+
+    private static Colors tapeColor = Colors.GREEN;
+    private static int turnAngle = 10;
+
+    public LineFollow() {
+        super("Lawn Mower [sol]");
+    }
+
+  @Override
+    public void run() {
+        while(!isInterrupted()){
+            setMotorSpeed(200);
+            forward();
+            if(getLeftColor()== tapeColor){
+                turnLeft(turnAngle);
+            }else if(getRightColor()== tapeColor){
+                turnRight(turnAngle);
+            }
+            delay(50);
+        }
+    }
+}
